319c9713f4548594c81825442c8fbb95c46cefe4
Nilesh Malviya <nilesh.malviya.er@gmail.com>
2022-05-08 20:29:48 +0530

    pass mobile number as parameter in patientProfileGetData in method patientProfileSettingShowData for getting correct data for login patient

--- src/main/java/com/Apointment/Controller/PatientController.java
+++ src/main/java/com/Apointment/Controller/PatientController.java
@@ -19,177 +19,217 @@ import com.Apointment.Model.UserDAOimp;
  */
 @WebServlet("/PatientController")
 public class PatientController extends HttpServlet {
-private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-UserDAOimp udi= null;
-PatientDAOimp pdi =null;
+	UserDAOimp udi= null; 
+	PatientDAOimp pdi =null;
     public PatientController() {
-    pdi=new PatientDAOimp();
-    udi= new UserDAOimp();
-   
+    	pdi=new PatientDAOimp();
+    	udi= new UserDAOimp();
+    	
         // TODO Auto-generated constructor stub
     }
 
-/**
-* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-*/
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-// TODO Auto-generated method stub
-doPost(request, response);
-response.getWriter().append("Served at: ").append(request.getContextPath());
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+	} 
 
-}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String action= request.getParameter("action");
+		
+		switch(action) {
+		case "register":  patientRegistration(request,response);break;
+		
+		case "profileSettingInsData": patientProfileSettingInsData(request,response);break;
+		
+		case "profileSettingShowData": patientProfileSettingShowData(request,response);break;
+		
+		
+		default :
+		
+		}
 
-/**
-* @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-*/
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+		
+	}
+	
+	protected void patientProfileSettingShowData(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		String mobileNumber=request.getParameter("mobile");
+		 PatientSettingData psd= pdi.patientProfileGetData(mobileNumber);
+		  request.setAttribute("patient",psd);
+		  RequestDispatcher rd = request.getRequestDispatcher("profile-settings.jsp");
+		  rd.forward(request, response);
+		
+	}
+	
+	
+	protected void patientProfileSettingInsData(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String dateOfBirth = request.getParameter("dateOfBirth");
+		String bloodGroup = request.getParameter("bloodGroup");
+		String emailId = request.getParameter("emailId");
+		String mobile = request.getParameter("mobile");
+		String address = request.getParameter("address");
+		String city = request.getParameter("city");
+		String state = request.getParameter("state");
+		String zipCode = request.getParameter("zipCode");
+		String country = request.getParameter("country");
+		
+		firstName=firstName.trim();
+		lastName=lastName.trim();
+		dateOfBirth=dateOfBirth.trim();
+		bloodGroup=bloodGroup.trim();
+		emailId=emailId.trim();
+		mobile = mobile.trim();
+		//mobile=(String) request.getAttribute("mobile");
+		
+		System.out.print(mobile);
+		
+		
+		address =address.trim();
+		city =city.trim();
+		state = state.trim();
+		zipCode = zipCode.trim();
+		country= country.trim();
+		
+		
+		if(firstName=="" ||lastName=="" ||dateOfBirth=="" || bloodGroup=="" || emailId=="" ||  address=="" || city=="" || state=="" || zipCode=="" || country=="" ) {
+			
+			request.setAttribute("msg","Please Enter details");
+			patientProfileSettingShowData(request,response); 
+		
+		}else {
+		
+		  PatientSettingData psd = new PatientSettingData();
+		  
+		  psd.setFirstName(firstName); 
+		  psd.setLastName(lastName);
+		  psd.setDateOfBirth(dateOfBirth);
+		  psd.setBloodGroup(bloodGroup);
+		  psd.setEmailId(emailId);
+		  psd.setMobile(mobile);
+		  psd.setAddress(address);
+		  psd.setCity(city);
+		  psd.setState(state);
+		  psd.setZipCode(zipCode);
+		  psd.setCountry(country);
+		  int check=0;
+			
+		  
+		  check= pdi.patientProfileInsData(psd); 
+			
+		  
+		  
+		  if(check==1) {
+			  request.setAttribute("msg1","Your data updated succesfully");
+			 
+			  patientProfileSettingShowData(request,response);
+			  
+			/*  
+			  RequestDispatcher rd = request.getRequestDispatcher("profile-settings.jsp");
+			  rd.forward(request, response);*/
+			  
+		  }else {
+			  request.setAttribute("msg","This mobile number is already registerd");
+			  RequestDispatcher rd = request.getRequestDispatcher("profile-settings.jsp");
+			  rd.forward(request, response);
+		  }
+		  
+		  
+		}
+		
+		  
+	}
+	
+	protected void patientRegistration(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		String fname = request.getParameter("fname");
+		String lname = request.getParameter("lname");
+		String mobileNumber = request.getParameter("mobileNumber");
+		String password=request.getParameter("password");
+	try {
+		fname=fname.trim();
+		lname=lname.trim();
+		mobileNumber=mobileNumber.trim();
+		password=password.trim();
+		
+		if( fname==""|| lname=="" || mobileNumber=="" ||password=="" ) {
+			request.setAttribute("loginError", "Please Enter details !!!!!!");
+			RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
+			rd.forward(request, response);
+			
+		}else if(isValid(mobileNumber)) {
 
-String action= request.getParameter("action");
-
-switch(action) {
-case "register":  patientRegistration(request,response);break;
-
-case "profileSettingInsData": patientProfileSettingInsData(request,response);break;
-
-case "profileSettingShowData": patientProfileSettingShowData(request,response);break;
-
-
-default :
-
-}
-
-
-
-}
-
-protected void patientProfileSettingShowData(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-PatientSettingData psd= pdi.patientProfileGetData();
- request.setAttribute("patient",psd);
- RequestDispatcher rd = request.getRequestDispatcher("profile-settings.jsp");
- rd.forward(request, response);
-
-}
-
-
-protected void patientProfileSettingInsData(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-String firstName = request.getParameter("firstName");
-String lastName = request.getParameter("lastName");
-String dateOfBirth = request.getParameter("dateOfBirth");
-String bloodGroup = request.getParameter("bloodGroup");
-String emailId = request.getParameter("emailId");
-String mobile = request.getParameter("mobile");
-String address = request.getParameter("address");
-String city = request.getParameter("city");
-String state = request.getParameter("state");
-String zipCode = request.getParameter("zipCode");
-String country = request.getParameter("country");
-String checkup = request.getParameter("update");
-
-
- PatientSettingData psd = new PatientSettingData();
- 
- psd.setFirstName(firstName);
- psd.setLastName(lastName);
- psd.setDateOfBirth(dateOfBirth);
- psd.setBloodGroup(bloodGroup);
- psd.setEmailId(emailId);
- psd.setMobile(mobile);
- psd.setAddress(address);
- psd.setCity(city);
- psd.setState(state);
- psd.setZipCode(zipCode);
- psd.setCountry(country);
- int check=0;
-/*
-* if(checkup=="update") {
-*
-*
-* }else { check= pdi.patientProfileInsData(psd); }
-*/
- 
- 
- if(check==1) {
- request.setAttribute("msg1","Your data updated succesfully");
- RequestDispatcher rd = request.getRequestDispatcher("profile-settings.jsp");
- rd.forward(request, response);
- 
- }else {
- request.setAttribute("msg","This mobile number is already registerd");
- RequestDispatcher rd = request.getRequestDispatcher("profile-settings.jsp");
- rd.forward(request, response);
- }
- 
-}
-
-protected void patientRegistration(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-String name = request.getParameter("name");
-String mobileNumber = request.getParameter("mobileNumber");
-String password=request.getParameter("password");
-
-name=name.trim();
-mobileNumber=mobileNumber.trim();
-password=password.trim();
-
-if(name=="" || mobileNumber=="" ||password=="" ) {
-request.setAttribute("loginError", "Please Enter details !!!!!!");
-RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
-rd.forward(request, response);
-}else if(isValid(mobileNumber)) {
-
-UserData ud = new UserData();
-ud.setName(name);
-ud.setMobileNumber(mobileNumber);
-ud.setPassword(password);
-ud.setType(0);
-
-
-// System.out.println(name+"  "+"  "+mobileNumber+"  "+password);
-udi.addUserData(ud);
-
-RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
-rd.forward(request, response);
-
-}else{
-
-request.setAttribute("loginError", "Please Enter Valid mobile number !!!!!!");
-RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
-rd.forward(request, response);
-}
-// TODO Auto-generated method stub
-
-
-}
-
-
-
-public static boolean isValid(String s)
-   {
-
-       // The given argument to compile() method
-       // is regular expression. With the help of
-       // regular expression we can validate mobile
-       // number.
-       // The number should be of 10 digits.
-
-       // Creating a Pattern class object
-       Pattern p = Pattern.compile("^\\d{10}$");
-
-       // Pattern class contains matcher() method
-       // to find matching between given number
-       // and regular expression for which
-       // object of Matcher class is created
-       java.util.regex.Matcher m = p.matcher(s);
-
-       // Returning boolean value
-       return (m.matches());
-   }
-
+			UserData ud = new UserData();
+			ud.setFname(fname);
+			ud.setLname(lname);
+			ud.setMobileNumber(mobileNumber);
+			ud.setPassword(password);
+			ud.setType(0);
+			
+			
+//			System.out.println(name+"  "+"  "+mobileNumber+"  "+password);
+			udi.addUserData(ud);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+			rd.forward(request, response);
+		
+		}else{
+			
+			request.setAttribute("loginError", "Please Enter Valid mobile number !!!!!!");
+			RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
+			rd.forward(request, response);
+		}
+		
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+	
+	
+	 public static boolean isValid(String s)
+	    {
+	 
+	        // The given argument to compile() method
+	        // is regular expression. With the help of
+	        // regular expression we can validate mobile
+	        // number.
+	        // The number should be of 10 digits.
+	 
+	        // Creating a Pattern class object
+	        Pattern p = Pattern.compile("^\\d{10}$");
+	 
+	        // Pattern class contains matcher() method
+	        // to find matching between given number
+	        // and regular expression for which
+	        // object of Matcher class is created
+	        java.util.regex.Matcher m = p.matcher(s);
+	 
+	        // Returning boolean value
+	        return (m.matches());
+	    }
+	
 
 }
